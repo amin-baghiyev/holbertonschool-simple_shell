0f93a01baf61d291c0e4732947583ea1c847002a
amin-baghiyev <baghiyev.amin@gmail.com>
2025-10-04 16:57:48 +0000

    Simple shell 0.3 - trim updated

--- trim.c
+++ trim.c
@@ -9,14 +9,15 @@
  */
 char *trim(char *s)
 {
-	size_t len = strlen(s), start = 0, end = len - 1;
+	char *end;
 
-	while (s[start] == ' ' || s[start] == '\t' || s[start] == '\n')
-		start++;
-	while (end > start && (s[end] == ' ' || s[end] == '\t' || s[end] == '\n'))
+	while (*s == ' ' || *s == '\t')
+		s++;
+	if (*s == '\0')
+		return (s);
+	end = s + strlen(s) - 1;
+	while (end > s && (*end == ' ' || *end == '\t'))
 		end--;
-	if (start > 0)
-		memmove(s, s + start, end - start + 1);
-	s[end - start + 1] = '\0';
+	*(end + 1) = '\0';
 	return (s);
 }
